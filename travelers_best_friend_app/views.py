16d2e906fe58153b03124805e446c28001357550
mariussanta <mariuss.santa@gmail.com>
2020-12-11 19:03:32 +0200

    test push 0

--- travelers_best_friend_app/views.py
+++ travelers_best_friend_app/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def homepage(request):
-    return HttpResponse('<h1>Travel\'s best friend</h1>')
+    return HttpResponse('<h1>Travel\'s best friend!</h1>')
