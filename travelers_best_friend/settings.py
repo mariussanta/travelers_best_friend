b6554e17a7a6a6efe8e200ab3003523a0c9c70e6
mariussanta <mariuss.santa@gmail.com>
2020-12-11 18:57:26 +0200

    created a basic homepage

--- travelers_best_friend/settings.py
+++ travelers_best_friend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'travelers_best_friend_app',
 ]
 
 MIDDLEWARE = [
---
+++ travelers_best_friend_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TravelersBestFriendAppConfig(AppConfig):
+    name = 'travelers_best_friend_app'
