7f6af69566b16fb16351e8a764f50a8a0cbf43f6
mariussanta <mariuss.santa@gmail.com>
2020-12-11 17:48:59 +0200

    first commit

---
+++ travelers_best_friend/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for travelers_best_friend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travelers_best_friend.settings')
+
+application = get_asgi_application()
