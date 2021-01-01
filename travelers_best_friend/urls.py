e1b6f798f0c74945c970faa2a88dffa7b7439c5f
mariussanta <mariuss.santa@gmail.com>
2021-01-01 16:22:53 +0200

    ulrs restructured

--- travelers_best_friend/urls.py
+++ travelers_best_friend/urls.py
@@ -15,33 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.views.generic import RedirectView
 
-from users_app import views as user_app_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # user paths
-    path('register/', user_app_views.register, name='register'),
-    path('profile/', user_app_views.profile, name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name='users_app/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users_app/logout.html'), name='logout'),
-    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users_app/password_reset.html'),
-         name='password_reset'),
-    path('password-reset/done/',
-         auth_views.PasswordResetDoneView.as_view(template_name='users_app/password_reset_done.html'),
-         name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(template_name='users_app/password_reset_confirm.html'),
-         name='password_reset_confirm'),
-    path('password-reset-complete/',
-         auth_views.PasswordResetCompleteView.as_view(template_name='users_app/password_reset_complete.html'),
-         name='password_reset_complete'),
-
-
+    path('', include('users_app.urls')),
     path('', include('travelers_best_friend_app.urls')),
 
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/images/favicon.ico'))
