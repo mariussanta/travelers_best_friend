bb82816c23a420f8e150e7be53108adf4e6e595c
mariussanta <mariuss.santa@gmail.com>
2021-01-03 18:21:40 +0200

    user page

--- travelers_best_friend/urls.py
+++ travelers_best_friend/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -27,3 +29,7 @@ urlpatterns = [
 
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/images/favicon.ico'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
